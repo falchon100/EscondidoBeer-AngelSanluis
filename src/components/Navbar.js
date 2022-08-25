318351e5bbef68183fd8a38aff76339b2c157bf5
Angel San Luis <94204485+falchon100@users.noreply.github.com>
2022-08-24 22:42:55 -0300

    entrega Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,11 +3,11 @@ import "../style/navbar.css"
 export function Navbar(){
     return (
     <nav className="nav container-fluid">
-        <a className="nav_a_contenedor"> <img className="nav_img" src={require("../image/1.jpg") } alt="" /> </a>
+        <a href="/" className="nav_a_contenedor"> <img className="nav_img" src={require("../image/1.jpg") } alt="" /> </a>
      <ul className="nav_ul" >
-        <li className="nav_li">Productos</li>
-        <li className="nav_li">Contacto</li>
-        <li className="nav_li">Pedido Online</li>
+        <li className="nav_li"><a href="/" className="nav_a">Productos</a></li>
+        <li className="nav_li"><a href="/" className="nav_a">Contacto</a></li>
+        <li className="nav_li"><a href="/" className="nav_a" >Pedido Online</a></li>
      </ul>
     <i class="fa-solid fa-bars nav_hamb"></i>
     </nav>)}
