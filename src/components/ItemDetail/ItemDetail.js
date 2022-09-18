9395d3a3ee828c5f4e8d556c91f3c6f96484f06c
Angel San Luis <94204485+falchon100@users.noreply.github.com>
2022-09-18 17:55:38 -0300

    entrega Boton ItemCount

--- src/components/ItemDetail/ItemDetail.js
+++ src/components/ItemDetail/ItemDetail.js
@@ -1,9 +1,14 @@
 import "./ItemDetail.css";
 import { ItemCount } from "../ItemCount/ItemCount";
+import { useState } from "react";
+import { Link } from "react-router-dom";
 
 const ItemDetail = ({ lista }) => {
+  const [goToCart, setGoToCart] = useState(true);
+
   const agregar = (cantidad) => {
-    alert(`Agregaste ${cantidad} productos al carrito`);
+    /*   alert(`Agregaste ${cantidad} productos al carrito`); */
+    setGoToCart(false);
   };
   console.log(lista);
   return (
@@ -13,7 +18,14 @@ const ItemDetail = ({ lista }) => {
         <h2>{lista.title} </h2>
         <h3>$ {lista.precio} </h3>
         <img width="200px" alt={lista.title} src={lista.img} />
-        <ItemCount initial={1} stock={lista.stock} onAdd={agregar} />
+        {goToCart ? (
+          <ItemCount initial={1} stock={lista.stock} onAdd={agregar} />
+        ) : (
+          <>
+            <Link to={"/cart"}>Finalizar Compra</Link>
+            <Link to={"/"}> Volver </Link>{" "}
+          </>
+        )}
       </div>
     </>
   );
